3bdf1c311e333503f19f4e4354a23e55557a72d5
trotrem <dgici@hotmail.com>
2015-10-13 18:08:14 -0400

    tadaa

--- Poly-Games/Assets/Camera2_follow.cs
+++ Poly-Games/Assets/Camera2_follow.cs
@@ -5,26 +5,34 @@ public class Camera2_follow : MonoBehaviour
 {
 
 
-    public GameObject player2;
+    public GameObject player;
     public float distance = 10.0f, smoothFactor = 5f, width = 256f, height = 256f;
-    Vector3 velocity = Vector3.zero;
+    Vector3 velocity, screenPos;
+    Camera aura;
+    public Camera main;
+
 
 
     void Start()
     {
+        aura = GetComponent<Camera>();
 
     }
 
 
     void Update()
     {
-        if (player2)
+        screenPos = main.WorldToScreenPoint(player.transform.position);
+        aura.rect = new Rect((screenPos.x) / Screen.width - width/2,  (screenPos.y) / Screen.height - height/2, width, height);
+        /*
+        if (player)
         {
-            velocity = new Vector3((player2.transform.position.x - transform.position.x) / smoothFactor,
-                               (player2.transform.position.y - transform.position.y) / smoothFactor,
+            velocity = new Vector3((player.transform.position.x - transform.position.x) / smoothFactor,
+                               (player.transform.position.y - transform.position.y) / smoothFactor,
                                 0);
             transform.position += velocity;
         }
+        */
     }
 }
 
